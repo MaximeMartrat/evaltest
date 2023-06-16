e962b43080e7cac052502f8f9102de86d0eb54f1
MaximeMartrat <maximemartrat@outlook.fr>
2023-06-16 17:14:21 +0200

    refactor maj

--- python-api/app.py
+++ python-api/app.py
@@ -1,9 +1,13 @@
-from flask import Flask,  render_template, request
+from flask import Flask, render_template, request
 from DAO.adminDAO import insertAdmin
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+     return render_template('accueil.html')
+
+@app.route('/admin')
 def post():
     return render_template('form.html')
 
@@ -11,7 +15,8 @@ def post():
 def form():
     if request.method == 'POST':
         insertAdmin()
-        return 'insertion reussie'
+        return render_template('accueil.html', message='Admin créé !')
+    
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
 
