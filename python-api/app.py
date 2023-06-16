f3e00bdf8be98af3c7032b4f3f3960b5d7675a4b
MaximeMartrat <maximemartrat@outlook.fr>
2023-06-16 15:19:38 +0200

    form3

--- python-api/app.py
+++ python-api/app.py
@@ -29,29 +29,36 @@ def form():
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
         if request.form['formateur'] == 'y':
-             sql_request2 = "INSERT INTO formateur (nomFormateur, prenomFormateur, pseudoFormateur, emailFormateur, passwordFormateur, id_section_FK) values (%s, %s, %s, %s, %s, %s)"
-             params2 = (nom, prenom, pseudo, email, hashed_password, section)
-             with mysql.connector.connect(**connection_params) as db:
-                with db.cursor() as c:
-                    c.execute(sql_request2, params2)
-                    db.commit()
-                    sql_request3 = "SELECT id_formateur FROM formateur WHERE nomFormateur=%s AND prenomFormateur=%s"
-                    params3 = (nom, prenom)
-                    c.execute(sql_request3, params3)
-                    result = c.fetchone()
-                    id_formateur = result[0]
-                    if id_formateur:             
+            sql_request2 = "INSERT INTO formateur (nomFormateur, prenomFormateur, pseudoFormateur, emailFormateur, passwordFormateur, id_section_FK) values (%s, %s, %s, %s, %s, %s)"
+            params2 = (nom, prenom, pseudo, email, hashed_password, section)
+            try:
+                with mysql.connector.connect(**connection_params) as db:
+                    with db.cursor() as c:
+                        c.execute(sql_request2, params2)
+                        db.commit()
+                        sql_request3 = "SELECT id_formateur FROM formateur WHERE nomFormateur=%s AND prenomFormateur=%s"
+                        params3 = (nom, prenom)
+                        c.execute(sql_request3, params3)
+                        result = c.fetchone()
+                        id_formateur = result[0]             
                         sql_request = "INSERT INTO administrateur (nomAdmin, prenomAdmin, pseudoAdmin, emailAdmin, telephoneAdmin, passwordAdmin, id_formateur_FK, id_section_FK) values (%s, %s, %s, %s, %s, %s, %s, %s)"
                         params = (nom, prenom, pseudo, email, telephone, hashed_password, id_formateur, section)
-                    else :
-                        sql_request = "INSERT INTO administrateur (nomAdmin, prenomAdmin, pseudoAdmin, emailAdmin, telephoneAdmin, passwordAdmin,  id_section_FK) values (%s, %s, %s, %s, %s, %s, %s)"
-                        params = (nom, prenom, pseudo, email, telephone, hashed_password, section)
-                    c.execute(sql_request, params)
-                    db.commit()
-                    return 'insertion réussie'
+                        c.execute(sql_request, params)
+                        db.commit()
+                        return 'insertion réussie'
+            except mysql.connector.Error as error:
+                    print("Failed to execute query: {}".format(error))
         else:
-            return 'insertion ratée'
-        
+            sql_request = "INSERT INTO administrateur (nomAdmin, prenomAdmin, pseudoAdmin, emailAdmin, telephoneAdmin, passwordAdmin,  id_section_FK) values (%s, %s, %s, %s, %s, %s, %s)"
+            params = (nom, prenom, pseudo, email, telephone, hashed_password, section)
+            try:
+                with mysql.connector.connect(**connection_params) as db:
+                    with db.cursor() as c:
+                        c.execute(sql_request, params)
+                        db.commit()
+                        return 'insertion réussie'
+            except mysql.connector.Error as error:
+                 print("Failed to execute query: {}".format(error))
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
