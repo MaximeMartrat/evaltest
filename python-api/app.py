5d5d0b22ea30a11ee06421b196d5517d71733af1
MaximeMartrat <maximemartrat@outlook.fr>
2023-06-16 14:58:49 +0200

    form2

--- python-api/app.py
+++ python-api/app.py
@@ -14,7 +14,6 @@ connection_params = {
     'password': "password",
     'database': "db",
 }
-sql_request = "INSERT INTO administrateur (nomAdmin, prenomAdmin, pseudoAdmin, emailAdmin, telephoneAdmin, passwordAdmin, id_section_FK) values (%s, %s, %s, %s, %s, %s, %s)"
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
@@ -29,22 +28,29 @@ def form():
 
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-        params = (nom, prenom, pseudo, email, telephone, hashed_password, section)
         if request.form['formateur'] == 'y':
              sql_request2 = "INSERT INTO formateur (nomFormateur, prenomFormateur, pseudoFormateur, emailFormateur, passwordFormateur, id_section_FK) values (%s, %s, %s, %s, %s, %s)"
              params2 = (nom, prenom, pseudo, email, hashed_password, section)
              with mysql.connector.connect(**connection_params) as db:
                 with db.cursor() as c:
                     c.execute(sql_request2, params2)
-                    db.commit()        
-        
-        with mysql.connector.connect(**connection_params) as db:
-            with db.cursor() as c:
-                c.execute(sql_request, params)
-                db.commit()
-                return 'insertion réussie'
-    else:
-        return 'insertion ratée'
+                    db.commit()
+                    sql_request3 = "SELECT id_formateur FROM formateur WHERE nomFormateur=%s AND prenomFormateur=%s"
+                    params3 = (nom, prenom)
+                    c.execute(sql_request3, params3)
+                    result = c.fetchone()
+                    id_formateur = result[0]
+                    if id_formateur:             
+                        sql_request = "INSERT INTO administrateur (nomAdmin, prenomAdmin, pseudoAdmin, emailAdmin, telephoneAdmin, passwordAdmin, id_formateur_FK, id_section_FK) values (%s, %s, %s, %s, %s, %s, %s, %s)"
+                        params = (nom, prenom, pseudo, email, telephone, hashed_password, id_formateur, section)
+                    else :
+                        sql_request = "INSERT INTO administrateur (nomAdmin, prenomAdmin, pseudoAdmin, emailAdmin, telephoneAdmin, passwordAdmin,  id_section_FK) values (%s, %s, %s, %s, %s, %s, %s)"
+                        params = (nom, prenom, pseudo, email, telephone, hashed_password, section)
+                    c.execute(sql_request, params)
+                    db.commit()
+                    return 'insertion réussie'
+        else:
+            return 'insertion ratée'
         
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
